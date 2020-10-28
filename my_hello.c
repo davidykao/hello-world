2453230d5f7c9af09f785f2686dd25864b7a1a08
davidykao <73617238+davidykao@users.noreply.github.com>
2020-10-28 15:33:52 -0700

    Create new my_hello.c
    Just a test program

---
+++ my_hello.c
@@ -0,0 +1,10 @@
+/* Author : David Kao */
+/* Copyright David Kao 2020 */
+
+#include <stdio.h>
+
+int main(int argc, char **argv ) {
+  printf("\n Hello World from David Kao!\n\n");
+  
+  return(0);
+}
